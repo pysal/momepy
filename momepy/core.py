32e7d022481ed807cc36df9c9d6f0625686fa366
Martin Fleischmann <martin@martinfleischmann.net>
2018-05-17 18:13:48 +0100

    added building_area

--- momepy/core.py
+++ momepy/core.py
@@ -1,1 +1,41 @@
-# Core of the momepy. From here everything will be called.
+'''
+Core of the momepy. From here everything will be called.
+
+Open/close calls on files always in core.py. Calculations always in helpers.
+'''
+import geopandas as gpd
+
+from .dimension import object_area
+
+
+# to be removed
+def gethead(path):
+    file = gpd.read_file(path)
+    show = file.head(5)
+    print(show)
+
+
+# dimension characters
+'''
+building_area:
+    character id: pdbAre
+
+    Return areas of buildings (or different elements, but then it is recommended
+    to change attribute column_name).
+
+    Attributes: path = path to file (tested on shapefile)
+                column_name = name of the column, default 'pdbAre' (optional)
+'''
+
+
+def building_area(path, column_name='pdbAre'):
+    print('Loading file.')
+    objects = gpd.read_file(path)  # load file into geopandas
+    print('Shapefile loaded.')
+
+    object_area(objects, column_name)  # call function object_area from dimension
+
+    # save dataframe back to file
+    print('Saving file.')
+    objects.to_file(path)
+    print('File saved.')
--- momepy/dimension.py
+++ momepy/dimension.py
@@ -1,2 +1,28 @@
 # dimension.py
 # definitons of dimension characters
+
+import geopandas as gpd
+from tqdm import tqdm  # progress bar
+
+'''
+object_area:
+    Calculate area of each object in given shapefile. It can be used for any
+    suitable element (building footprint, plot, voronoi cell, block).
+
+    Attributes: objects = geoDataFrame with objects
+                column_name = name of the column to save the area values
+'''
+
+
+def object_area(objects, column_name):
+
+    # define new column
+    objects[column_name] = None
+    objects[column_name] = objects[column_name].astype('float')
+    print('Column ready. Calculating.')
+
+    # fill new column with the value of area, iterating over rows one by one
+    for index, row in tqdm(objects.iterrows(), total=objects.shape[0]):
+        objects.loc[index, column_name] = row['geometry'].area
+
+    print('Areas calculated.')
