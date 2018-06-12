3d796cec2270b558c979191526d0f3b78d10fe7b
Martin Fleischmann <martin@martinfleischmann.net>
2018-06-12 19:36:00 +0100

    first steps for voronoi tesselation

--- momepy/elements.py
+++ momepy/elements.py
@@ -2,6 +2,9 @@
 # generating derived elements (street edge, block)
 
 import geopandas as gpd
+from tqdm import tqdm  # progress bar
+from osgeo import ogr
+from shapely.wkt import loads
 
 '''
 clean geometry
@@ -22,3 +25,36 @@ def clean_buildings(path, height_column):
     print('Saving file.')
     objects.to_file(path)
     print('File saved.')
+
+'''
+Voronoi tesselation as a substitute of morphological plot
+
+input layer: building footprints
+'''
+
+path = "/Users/martin/Strathcloud/Personal Folders/Test data/Royston/buildings.shp"
+print('Loading file.')
+objects = gpd.read_file(path)  # load file into geopandas
+print('Shapefile loaded.')
+
+objects['geometry'] = tqdm(objects.buffer(-0.1))  # change original geometry with buffered one
+
+
+# densify geometry beforo Voronoi tesselation
+def segmentize(geom):
+    wkt = geom.wkt  # shapely Polygon to wkt
+    geom = ogr.CreateGeometryFromWkt(wkt)  # create ogr geometry
+    geom.Segmentize(2)  # densify geometry by 2 metres
+    wkt2 = geom.ExportToWkt()  # ogr geometry to wkt
+    new = loads(wkt2)  # wkt to shapely Polygon
+    return new
+
+objects['geometry'] = tqdm(objects['geometry'].map(segmentize))
+
+
+
+objects.to_file("/Users/martin/Strathcloud/Personal Folders/Test data/Royston/Tess/buildings.shp")
+
+
+# Voronoi
+# dissolve
