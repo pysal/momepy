180d08e11522014ac663acecb4793768af29a844
Martin Fleischmann <martin@martinfleischmann.net>
2018-06-17 19:31:24 +0100

    Voronoi tesselation

--- momepy/elements.py
+++ momepy/elements.py
@@ -5,6 +5,10 @@ import geopandas as gpd
 from tqdm import tqdm  # progress bar
 from osgeo import ogr
 from shapely.wkt import loads
+import numpy as np
+from scipy.spatial import Voronoi, voronoi_plot_2d
+import matplotlib.pyplot as plt
+
 
 '''
 clean geometry
@@ -32,7 +36,7 @@ Voronoi tesselation as a substitute of morphological plot
 input layer: building footprints
 '''
 
-path = "/Users/martin/Strathcloud/Personal Folders/Test data/Royston/buildings.shp"
+path = "/Users/martin/Strathcloud/Personal Folders/Test data/Royston/Tess/bul.shp"
 print('Loading file.')
 objects = gpd.read_file(path)  # load file into geopandas
 print('Shapefile loaded.')
@@ -51,10 +55,25 @@ def segmentize(geom):
 
 objects['geometry'] = tqdm(objects['geometry'].map(segmentize))
 
+# Voronoi
+
+voronoi_points = np.empty([1, 2])
+voronoi_points
+
+for index, row in tqdm(objects.iterrows(), total=objects.shape[0]):
+    poly_ext = row['geometry'].exterior
+    point_coords = poly_ext.coords
+    row_array = np.array(point_coords)
+    voronoi_points = np.concatenate((voronoi_points, row_array))
+
+voronoi_diagram = Voronoi(voronoi_points[1:])
+
+voronoi_plot_2d(voronoi_diagram)
+# NEXT STEP = SAVE VORONOI TESSELATION TO SHAPEFILE
+# https://stackoverflow.com/questions/27548363/from-voronoi-tessellation-to-shapely-polygons
+
+
+# dissolve
 
 
 objects.to_file("/Users/martin/Strathcloud/Personal Folders/Test data/Royston/Tess/buildings.shp")
-
-
-# Voronoi
-# dissolve
