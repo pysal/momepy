c8a9936ccdded2f594c13d6aac197fe354dabd3f
Martin Fleischmann <martin@martinfleischmann.net>
2018-11-07 17:37:33 +0000

    comments

--- momepy/sandbox/segment_extension.py
+++ momepy/sandbox/segment_extension.py
@@ -6,11 +6,13 @@ import geopandas as gpd
 
 
 def snap_street_network(network, tolerance=20):
+    # extrapolating function - makes line as a extrapolation of existing with set length (tolerance)
     def getExtrapoledLine(p1, p2):
         'Creates a line extrapoled in p1->p2 direction'
-        EXTRAPOL_RATIO = tolerance
+        EXTRAPOL_RATIO = tolerance  # length of a line
         a = p2
 
+        # defining new point based on the vector between existing points
         if p1[0] >= p2[0] and p1[1] >= p2[1]:
             b = (p2[0] - EXTRAPOL_RATIO * math.cos(math.atan(math.fabs(p1[1] - p2[1] + 0.000001) / math.fabs(p1[0] - p2[0] + 0.000001))),
                  p2[1] - EXTRAPOL_RATIO * math.sin(math.atan(math.fabs(p1[1] - p2[1] + 0.000001) / math.fabs(p1[0] - p2[0] + 0.000001))))
@@ -25,6 +27,7 @@ def snap_street_network(network, tolerance=20):
                  p2[1] + EXTRAPOL_RATIO * math.sin(math.atan(math.fabs(p1[1] - p2[1] + 0.000001) / math.fabs(p1[0] - p2[0] + 0.000001))))
         return LineString([a, b])
 
+    # function extending line to closest object within set distance
     def extend_line():
         if Point(l_coords[-2]).distance(Point(l_coords[-1])) <= 0.001:
             extra = l_coords[-3:-1]
@@ -65,16 +68,19 @@ def snap_street_network(network, tolerance=20):
 
     file = network
 
+    # generating spatial index (rtree)
     sindex = file.sindex
 
+    # iterating over each street segment
     for idx, row in tqdm(file.iterrows(), total=file.shape[0]):
 
         line = row['geometry']
         l_coords = list(line.coords)
-        network = file.drop(idx, axis=0)['geometry']
+        network = file.drop(idx, axis=0)['geometry']  # ensure that it wont intersect itself
         start = Point(l_coords[0])
         end = Point(l_coords[-1])
 
+        # find out whether ends of the line are connected or not
         possible_first_index = list(sindex.intersection(start.bounds))
         possible_first_matches = file.iloc[possible_first_index]
         possible_first_matches_clean = possible_first_matches.drop(idx, axis=0)
@@ -85,16 +91,17 @@ def snap_street_network(network, tolerance=20):
         possible_second_matches_clean = possible_second_matches.drop(idx, axis=0)
         second = possible_second_matches_clean.intersects(end).any()
 
+        # both ends connected, do nothing
         if first == True and second == True:
             continue
-
+        # start connected, extend  end
         elif first == True and second == False:
             extend_line()
-
+        # end connected, extend start
         elif first == False and second == True:
             l_coords.reverse()
             extend_line()
-
+        # unconnected, extend both ends
         elif first == False and second == False:
             extend_line()
             l_coords.reverse()
