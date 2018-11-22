10b474e3516fff52e6f83e8e0c644d2b2d9eee25
Martin Fleischmann <martin@martinfleischmann.net>
2018-11-22 14:52:24 +0000

    get_network_id() based on street edges

--- momepy/elements.py
+++ momepy/elements.py
@@ -897,14 +897,14 @@ def get_network_id(buildings, streets, tesselation, unique_id_column, network_id
     def get_rtree(lines):
         def generate_items():
             sindx = 0
-            for lid, nid, l in tqdm(lines, total=len(lines)):
+            for nid, l in tqdm(lines, total=len(lines)):
                 for i in range(len(l) - 1):
                     a, b = l[i]
                     c, d = l[i + 1]
                     segment = ((a, b), (c, d))
                     box = (min(a, c), min(b, d), max(a, c), max(b, d))
                     # box = left, bottom, right, top
-                    yield (sindx, box, (lid, segment, nid))
+                    yield (sindx, box, (segment, nid))
                     sindx += 1
         return index.Index(generate_items())
 
@@ -916,7 +916,7 @@ def get_network_id(buildings, streets, tesselation, unique_id_column, network_id
             d = INFTY
             s = None
             for h in hits:
-                nearest_p, new_d = get_distance(p, h[1])
+                nearest_p, new_d = get_distance(p, h[0])
                 if d >= new_d:
                     d = new_d
                     # s = (h[0], h[1], nearest_p, new_d)
@@ -924,17 +924,6 @@ def get_network_id(buildings, streets, tesselation, unique_id_column, network_id
             result[p] = s
             if s is None:
                 result[p] = (0, 0)
-
-            # some checking you could remove after you adjust the constants
-            # if s is None:
-            #     raise Warning("It seems INFTY is not big enough. Point was not attached to street. It might be too far.", p)
-
-            # pboxpol = ((pbox[0], pbox[1]), (pbox[2], pbox[1]),
-            #            (pbox[2], pbox[3]), (pbox[0], pbox[3]))
-            # if not Polygon(pboxpol).intersects(LineString(s[1])):
-            #     msg = "It seems MIN_SIZE is not big enough. "
-            #     msg += "You could get inexact solutions if remove this exception."
-            #     raise Exception(msg)
 
         return result
 
@@ -952,7 +941,7 @@ def get_network_id(buildings, streets, tesselation, unique_id_column, network_id
     # make streets list for input
     street_list = []
     for idx, row in tqdm(streets.iterrows(), total=streets.shape[0]):
-        street_list.append((row[street_name_column], row[network_id_column], list(row['geometry'].coords)))
+        street_list.append((row[network_id_column], list(row['geometry'].coords)))
     print('Generating rtree...')
     idx = get_rtree(street_list)
 
@@ -960,7 +949,7 @@ def get_network_id(buildings, streets, tesselation, unique_id_column, network_id
     solutions = get_solution(idx, centroid_list)
 
     print('Forming DataFrame...')
-    df = pd.DataFrame.from_dict(solutions, orient='index', columns=['street', 'unused', network_id_column])  # solutions dict to df
+    df = pd.DataFrame.from_dict(solutions, orient='index', columns=['unused', 'unused', network_id_column])  # solutions dict to df
     df['point'] = df.index  # point to column
     df = df.reset_index()
     df['idx'] = df.index
@@ -969,83 +958,17 @@ def get_network_id(buildings, streets, tesselation, unique_id_column, network_id
     print('Joining DataFrames...')
     joined = buildings_c.merge(df, on='idx')
     print('Cleaning DataFrames...')
-    cleaned = joined[[unique_id_column, 'street', network_id_column]]
+    cleaned = joined[[unique_id_column, network_id_column]]
 
     print('Merging with tesselation...')
     tesselation = tesselation.merge(cleaned, on=unique_id_column)
-
-    print('Defining merge ID...')
-    for idx, row in tqdm(tesselation.iterrows(), total=tesselation.shape[0]):
-        tesselation.loc[idx, 'mergeID'] = str(row['street']) + str(row[block_id_column])
-
-    print('Dissolving...')
-    edges = tesselation.dissolve(by='mergeID')
-
-    # multipart geometry to singlepart
-    def multi2single(gpdf):
-        gpdf_singlepoly = gpdf[gpdf.geometry.type == 'Polygon']
-        gpdf_multipoly = gpdf[gpdf.geometry.type == 'MultiPolygon']
-
-        for i, row in gpdf_multipoly.iterrows():
-            Series_geometries = pd.Series(row.geometry)
-            df = pd.concat([gpd.GeoDataFrame(row, crs=gpdf_multipoly.crs).T] * len(Series_geometries), ignore_index=True)
-            df['geometry'] = Series_geometries
-            gpdf_singlepoly = pd.concat([gpdf_singlepoly, df])
-
-        gpdf_singlepoly.reset_index(inplace=True, drop=True)
-        return gpdf_singlepoly
-
-    edges_single = multi2single(edges)
-    edges_single['geometry'] = edges_single.exterior
-    print('Generating unique edge ID...')
-    id = 1
-    for idx, row in tqdm(edges_single.iterrows(), total=edges_single.shape[0]):
-        edges_single.loc[idx, 'eID'] = id
-        id = id + 1
-        edges_single.loc[idx, 'geometry'] = Polygon(row['geometry'])
-
-    edges_clean = edges_single[['geometry', 'eID', block_id_column]]
-
-    print('Isolating islands...')
-    sindex = edges_clean.sindex
-    islands = []
-    for idx, row in edges_clean.iterrows():
-        possible_matches_index = list(sindex.intersection(row['geometry'].bounds))
-        possible_matches = edges_clean.iloc[possible_matches_index]
-        possible_matches = possible_matches.drop([idx], axis=0)
-        if possible_matches.contains(row['geometry']).any():
-            islands.append(idx)
-
-    edges_clean = edges_clean.drop(islands, axis=0)
-    print(len(islands), 'islands deleted.')
-    print('Cleaning edges...')
-    edges_clean['geometry'] = edges_clean.buffer(0.000000001)
-
-    print('Saving street edges to', save_to)
-    edges_clean.to_file(save_to)
-
-    print('Cleaning tesselation...')
-    tesselation = tesselation.drop(['street', 'mergeID'], axis=1)
-
-    print('Tesselation spatial join [1/3]...')
-    tess_centroid = tesselation.copy()
-    tess_centroid['geometry'] = tess_centroid.centroid
-
-    edg_join = edges_clean.drop(['bID'], axis=1)
-
-    print('Tesselation spatial join [2/3]...')
-    tess_with_eID = gpd.sjoin(tess_centroid, edg_join, how='inner', op='intersects')
-    tess_with_eID = tess_with_eID[['uID', 'eID']]
-
-    print('Tesselation spatial join [3/3]...')
-    tesselation = tesselation.merge(tess_with_eID, on='uID')
 
     print('Saving tesselation to', tesselation_to)
     tesselation.to_file(tesselation_to)
 
     print('Buildings attribute join...')
     # attribute join cell -> building
-    tess_nid_eid = tesselation[['uID', 'eID', 'nID']]
+    tess_nid_eid = tesselation[['uID', 'nID']]
 
     buildings = buildings.merge(tess_nid_eid, on='uID')
 
