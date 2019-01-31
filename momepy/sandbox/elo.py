96c4df64d86d0462f0f4e655a7541ec75ff031c2
Martin Fleischmann <martin@martinfleischmann.net>
2019-01-31 17:12:01 +0000

    untrack sandbox

--- momepy/sandbox/elo.py
+++
@@ -1,8 +0,0 @@
-import math
-p = 315.9805592320672
-a = 6240.2321132881225
-elo1 = ((p - math.sqrt((p ** 2) - (16 * a))) / 4) / ((p / 2) - ((p - math.sqrt(p ** 2 - 16 * a)) / 4))
-p ** 2
-16 * a
-math.sqrt(0)
-elo1 = ((p - math.sqrt((p ** 2) - (16 * a))) / 4) / ((p / 2) - ((p - math.sqrt(p ** 2 - 16 * a)) / 4))
