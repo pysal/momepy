11ca88e8b3fa8f3ea0fbf9aae8191f2714ef054d
Martin Fleischmann <martin@martinfleischmann.net>
2019-02-09 23:23:24 +0000

    get_path and .avaialble for momepy.datasets (based on geopandas code)

---
+++ momepy/datasets/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+
+__all__ = ['available', 'get_path']
+
+_module_path = os.path.dirname(__file__)
+available = ['bubenec']
+
+
+def get_path(dataset):
+    """
+    Get the path to the data file.
+    Parameters
+    ----------
+    dataset : str
+        The name of the dataset. See ``momepy.datasets.available`` for
+        all options.
+    """
+    if dataset in available:
+        return os.path.abspath(
+            os.path.join(_module_path, dataset + '.gpkg'))
+    else:
+        msg = "The dataset '{data}' is not available. ".format(data=dataset)
+        msg += "Available datasets are {}".format(", ".join(available))
+        raise ValueError(msg)
