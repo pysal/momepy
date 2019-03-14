a5205d96c653d44a439d5ab10863c8f5f2822e7e
Martin Fleischmann <martin@martinfleischmann.net>
2019-03-14 17:30:52 +0000

    distribution test suite start

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -22,6 +22,9 @@ class TestShape:
         self.df_tessellation = gpd.read_file(test_file_path, layer='tessellation')
         self.df_buildings['height'] = np.linspace(10., 30., 144)
         self.df_buildings['volume'] = mm.volume(self.df_buildings, 'height')
+        self.df_streets['nID'] = mm.unique_id(self.df_streets)
+        self.df_buildings['nID'], self.df_tessellation['nID'] = mm.get_network_id(self.df_buildings, self.df_streets,
+                                                                                  'uID', 'nID', self.df_tessellation, 'uID')
 
     def test_orientation(self):
         self.df_buildings['orient'] = mm.orientation(self.df_buildings)
@@ -31,4 +34,10 @@ class TestShape:
     def test_shared_walls_ratio(self):
         self.df_buildings['swr'] = mm.shared_walls_ratio(self.df_buildings, 'uID')
         check = 0.3424804411228673
-        assert self.df_buildings['orient'][10] == check
+        assert self.df_buildings['swr'][10] == check
+
+    def test_street_alignment(self):
+        self.df_buildings['orient'] = mm.orientation(self.df_buildings)
+        self.df_buildings['street_alignment'] = mm.street_alignment(self.df_buildings, self.df_streets, 'orient', 'nID', 'nID')
+        check = 0.29073888476702336
+        assert self.df_buildings['street_alignment'][0] == check
