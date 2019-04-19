a6839d6243e838727551dea293cb20221fdff291
Martin Fleischmann <martin@martinfleischmann.net>
2019-04-19 17:24:22 +0100

    DOC: fix rtd

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@
 import os
 import sys
 import sphinx_bootstrap_theme
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath('.momepy'))
 
 
 # -- Project information -----------------------------------------------------
