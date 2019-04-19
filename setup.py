6fd0bc77c3629f827e63d1e0a49cdd6ab86d6656
Martin Fleischmann <martin@martinfleischmann.net>
2019-04-19 15:40:12 +0100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env/python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='momepy',
+      version='0.1a1',
+      description='Urban Morphology Measuring Toolkit',
+      license="MIT",
+      author='Martin Fleischmann',
+      author_email='martin@martinfleischmann.net',
+      url='https://github.com/martinfleis/momepy',
+      packages=['momepy'],
+      install_requires=['geopandas'],
+      )
