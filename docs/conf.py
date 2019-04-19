14b9901756941cfa85b91e5a2c4925c2d5efdaf6
Martin Fleischmann <martin@martinfleischmann.net>
2019-04-19 22:27:33 +0100

    DOC: RTD

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,9 @@ import os
 import sys
 import sphinx_bootstrap_theme
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
+
+import momepy
 
 
 class Mock(object):
