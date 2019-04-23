1ceff747e19ac24c792a5df48eea11afed2b6042
Martin Fleischmann <martin@martinfleischmann.net>
2019-04-23 09:44:25 +0100

    setup.py datasets

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env/python
-
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+# get all data dirs in the datasets module
+data_files = []
+
+for item in os.listdir("momepy/datasets"):
+    if not item.startswith('__'):
+        if os.path.isdir(os.path.join("momepy/datasets/", item)):
+            data_files.append(os.path.join("datasets", item, '*'))
+        elif item.endswith('.zip'):
+            data_files.append(os.path.join("datasets", item))
 
 setup(name='momepy',
       version='0.1a1',
@@ -13,5 +22,6 @@ setup(name='momepy',
       author='Martin Fleischmann',
       author_email='martin@martinfleischmann.net',
       url='https://github.com/martinfleis/momepy',
-      packages=['momepy'],
+      packages=['momepy', 'momepy.datasets'],
+      package_data={'momepy': data_files},
       )
