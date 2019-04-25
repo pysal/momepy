2d197004ac10bfd8b70f0ea4a98eea2fd28757a7
Martin Fleischmann <martin@martinfleischmann.net>
2019-04-25 10:28:29 +0100

    TST: elements

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -22,3 +22,22 @@ class TestUtils:
     def test_snap_street_network_edge(self):
         snapped = mm.snap_street_network_edge(self.df_streets, self.df_buildings, self.df_tessellation, 20, 70)
         assert sum(snapped.geometry.length) == 5980.041004739526
+
+    def test_blocks(self):
+        buildings, cells, blocks = mm.blocks(self.df_tessellation, self.df_streets, self.df_buildings, 'bID', 'uID')
+        assert not buildings.bID.isna().any()
+        assert not cells.bID.isna().any()
+        assert len(blocks) == 8
+
+    def test_get_network_id(self):
+        self.df_streets['nID'] = range(len(self.df_streets))
+        buildings_id, tessellation_id = mm.get_network_id(self.df_buildings, self.df_streets, 'uID', 'nID', self.df_tessellation, 'uID')
+        assert not buildings_id.isna().any()
+        assert not tessellation_id.isna().any()
+
+    def test_get_node_id(self):
+        nx = mm.gdf_to_nx(self.df_streets)
+        nodes = mm.nx_to_gdf(nx, edges=False)
+        nodes['nodeid'] = range(len(nodes))
+        ids = mm.get_node_id(self.df_buildings, nodes, 'nodeid')
+        assert not ids.isna().any()
