6ee98909f667ec3aae9cbdef8cf6a4132acc577d
Martin Fleischmann <martin@martinfleischmann.net>
2019-10-11 14:21:47 +0100

    test

--- tests/test_dimension.py
+++ tests/test_dimension.py
@@ -170,6 +170,11 @@ class TestDimensions:
         assert results2.o[0] == 0.5535714285714286
         assert results2.hd[0] == 5.526848034418866
 
+        results3 = mm.StreetProfile(self.df_streets, self.df_buildings)
+        assert results3.w[0] == 67.563796073073
+        assert results3.wd[0] == 8.791875291865827
+        assert results3.o[0] == 0.5535714285714286
+
     def test_WeightedCharacter(self):
         sw = sw_high(k=3, gdf=self.df_tessellation, ids="uID")
         weighted = mm.WeightedCharacter(self.df_buildings, "height", sw, "uID").wc
