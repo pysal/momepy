8d019b77a8184cbbbe92e271d68669a49bff180e
Martin Fleischmann <martin@martinfleischmann.net>
2019-11-10 16:49:15 +0000

    black

--- momepy/__init__.py
+++ momepy/__init__.py
@@ -12,5 +12,6 @@ __author__ = "Martin Fleischmann"
 __author_email__ = "martin@martinfleischmann.net"
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
