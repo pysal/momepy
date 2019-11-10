d01446db015cada517c6e2d32e653f9984386ea5
Martin Fleischmann <martin@martinfleischmann.net>
2019-11-10 18:44:11 +0000

    exclude from codecov

--- momepy/elements.py
+++ momepy/elements.py
@@ -19,7 +19,7 @@ try:
     from osgeo import ogr
 except ModuleNotFoundError:
     # nothing - temporary solution for readthedocs fail.
-    print('')
+    print("rtd")
 
 __all__ = ["buffered_limit", "Tessellation", "Blocks", "get_network_id", "get_node_id"]
 
