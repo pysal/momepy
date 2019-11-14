58a88daf1d4f8ffcf384e92de7b461012527f82e
Martin Fleischmann <martin@martinfleischmann.net>
2019-11-14 12:31:10 +0000

    move env files

--- .github/workflows/run_test.sh
+++
@@ -1,7 +0,0 @@
-. ./.github/workflows/set_os_env.sh
-
-echo "activate test-environment =================="
-source $MINICONDA_SUB_PATH/activate test
-$MINICONDA_SUB_PATH/conda list
-
-$MINICONDA_PYTEST --numprocesses=auto tests/
