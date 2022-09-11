30354ace63376612617461c67685bbe27a4b820e
Gabriel Agostini <gsagostini@gmail.com>
2022-09-11 12:52:52 -0400

    adding data for a street network

--- momepy/datasets/__init__.py
+++ momepy/datasets/__init__.py
@@ -3,10 +3,10 @@ import os
 __all__ = ["available", "get_path"]
 
 _module_path = os.path.dirname(__file__)
-available = ["bubenec", "tests"]
+available = ["bubenec", "tests", "nyc_graph"]
 
 
-def get_path(dataset):
+def get_path(dataset, extension='gpkg'):
     """
     Get the path to the data file.
     Parameters
@@ -14,9 +14,12 @@ def get_path(dataset):
     dataset : str
         The name of the dataset. See ``momepy.datasets.available`` for
         all options.
+    extension : str
+        The extension of the data file
     """
     if dataset in available:
-        return os.path.abspath(os.path.join(_module_path, dataset + ".gpkg"))
+        filepath=dataset+"."+extension
+        return os.path.abspath(os.path.join(_module_path, filepath))
     msg = "The dataset '{data}' is not available. ".format(data=dataset)
     msg += "Available datasets are {}".format(", ".join(available))
     raise ValueError(msg)
