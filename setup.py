d4e3a391d9c6291622f8d8b3e8a0940814aa7f36
James Gaboardi <jgaboardi@gmail.com>
2022-09-19 13:00:25 -0400

    migrate setup.cfg

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools import setup
 # https://github.com/python-versioneer/python-versioneer/issues/193
 sys.path.append(os.path.dirname(__file__))
 
+# see pyproject.toml for static project metadata
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
