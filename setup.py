a3ae17cef4e67d36b0457c78738a85fc7c03371c
James Gaboardi <jgaboardi@gmail.com>
2022-09-19 11:32:59 -0400

    add pyproject.toml [1]

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env/python
 import os
+import sys
 
 import versioneer
+from setuptools import setup
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# ensure the current directory is on sys.path so versioneer can be imported
+# when pip uses PEP 517/518 build rules.
+# https://github.com/python-versioneer/python-versioneer/issues/193
+sys.path.append(os.path.dirname(__file__))
+
 
 # get all data dirs in the datasets module
 data_files = []
