538bdee16a06b1ee75f67c9868ce28b53937806b
[greg maya] <gregoriomaya@gmail.com>
2023-02-08 11:01:27 +0100

    blank: line not containing whitespace?

--- momepy/preprocessing.py
+++ momepy/preprocessing.py
@@ -840,8 +840,8 @@ def _selecting_rabs_from_poly(
             rab_adj = gpd.sjoin(gdf, rab, predicate="intersects")
         else:
             rab_adj = gpd.sjoin(gdf, rab, op="intersects")
-            
-        #remove the adjacent polygons that are selected more than once
+
+        # remove the adjacent polygons that are selected more than once
         rab_adj = rab_adj[~rab_adj.index.duplicated(keep=False)]
 
         # selecting the adjacent polygons smaller than itself
