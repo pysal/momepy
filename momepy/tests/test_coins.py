b9ae7f24883508df4d3e5cbcaa1071e1a60d84b2
anastassiavybornova <anvy@itu.dk>
2024-06-27 19:26:46 +0200

    BUG: refactor computation of angles in COINS (#632)
    * reimplement vector-based interior angle computation
    
    * add tests for sharp angles
    
    * drop "orientation" col before asserting no NAs
    
    * add ValueError for overlapping/identical lines
    
    * update coins class docs
    
    * angle_threshold==180 is meaningless
    
    * add test case for angle_treshold > 0
    
    * ran black formatting
    
    * remove obsolete funcs
    
    * ran pre-commit & ran ruff & completely removed orientation func
    
    * Update momepy/coins.py
    
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>
    
    * Update momepy/coins.py
    
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>
    
    * Apply suggestions from code review
    
    * lint
    
    * revert linting changes
    
    * execute coins notebook
    
    * avoid numpy
    
    * streamline counting
    
    * consistency in imports
    
    * "a"
    
    ---------
    
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>

--- momepy/tests/test_coins.py
+++ momepy/tests/test_coins.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import pandas as pd
 import pytest
 from pandas.testing import assert_index_equal, assert_series_equal
+from shapely.geometry import LineString
 
 import momepy as mm
 
@@ -111,4 +112,108 @@ class TestCOINS:
         )
         assert_index_equal(result.columns, expected_columns)
 
-        assert not result.isna().any().any()
+        assert not result.drop(columns="orientation").isna().any().any()
+
+    def test_sharp_angles(self):
+        # test case 1
+        a = (0, 0)
+        b = (2, 0)
+        c = (1, 1)
+        d = (2, 1)
+        e = (-1, 1)
+
+        line1 = [a, b]
+        line2 = [a, c]
+        line3 = [a, d]
+        line4 = [a, e]
+
+        gdf = gpd.GeoDataFrame(
+            {
+                "geometry": [
+                    LineString(line1),
+                    LineString(line2),
+                    LineString(line3),
+                    LineString(line4),
+                ]
+            }
+        )
+
+        coins = mm.COINS(gdf, angle_threshold=0)
+        stroke_attr = coins.stroke_attribute()
+
+        expected_groups = [
+            bool(stroke_attr[0] == 0),
+            bool(stroke_attr[1] == 1),
+            bool(stroke_attr[2] == 2),
+            bool(stroke_attr[3] == 0),
+            bool(stroke_attr[0] != stroke_attr[2]),
+        ]
+
+        assert all(expected_groups)
+
+        # test case 2
+
+        a = (0, 0)
+        b = (-1, 1)
+        c = (-4, 1)
+        d = (-2, -1)
+        e = (-1, -2)
+
+        line1 = [a, b]
+        line2 = [a, c]
+        line3 = [a, d]
+        line4 = [a, e]
+
+        gdf = gpd.GeoDataFrame(
+            {
+                "geometry": [
+                    LineString(line1),
+                    LineString(line2),
+                    LineString(line3),
+                    LineString(line4),
+                ]
+            }
+        )
+
+        coins = mm.COINS(gdf, angle_threshold=0)
+        stroke_attr = coins.stroke_attribute()
+
+        expected_groups = [
+            bool(stroke_attr[0] == 0),
+            bool(stroke_attr[1] == 1),
+            bool(stroke_attr[2] == 2),
+            bool(stroke_attr[3] == 0),
+            bool(stroke_attr[0] != stroke_attr[2]),
+        ]
+
+        assert all(expected_groups)
+
+        # test case 3
+        a = (0, 0)
+        b = (1, 1)
+        c = (0, 1)
+
+        line1 = [a, b]
+        line2 = [a, c]
+
+        gdf = gpd.GeoDataFrame(
+            {
+                "geometry": [
+                    LineString(line1),
+                    LineString(line2),
+                ]
+            }
+        )
+
+        # interior angle is 45deg, angle_threshold is 0:
+        # expecting both lines to be part of same group
+        coins = mm.COINS(gdf, angle_threshold=0)
+        stroke_attr = coins.stroke_attribute()
+        assert bool(stroke_attr[0] == stroke_attr[1] == 0)
+
+        # interior angle is 45deg, angle_threshold is 46:
+        # expecting lines to in different groups
+        coins = mm.COINS(gdf, angle_threshold=46)
+        stroke_attr = coins.stroke_attribute()
+        # expecting both lines to be part of same group
+        assert stroke_attr[0] != stroke_attr[1]
