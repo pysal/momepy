7171afee6059992619bf240f01bd19cef28368cb
Martin Fleischmann <martin@martinfleischmann.net>
2024-07-25 16:46:11 +0200

    better option

--- docs/conf.py
+++ docs/conf.py
@@ -305,10 +305,11 @@ def linkcode_resolve(domain, info):
     return f"https://github.com/pysal/momepy/blob/{tag}/{filename}"
 
 
-# Set canonical URL from the Read the Docs Domain
+# Define the canonical URL if you are using a custom domain on Read the Docs
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
-html_context = {}
 
 # Tell Jinja2 templates the build is running on Read the Docs
 if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
     html_context["READTHEDOCS"] = True
