ed03a44c5a5aceb19432794b60e4df570950e4a9
Colin Watson <cjwatson@debian.org>
2025-04-22 12:22:34 +0100

    MAINT: add more pytest.approx calls
    These tests failed on Debian i386, probably due to x87 excess precision:
    https://bugs.debian.org/1103140

--- momepy/tests/test_dimension.py
+++ momepy/tests/test_dimension.py
@@ -215,7 +215,7 @@ class TestDimensions:
             self.df_streets, self.df_buildings, heights=height, tick_length=100
         )
         assert results2.w[0] == 70.7214870365335
-        assert results2.wd[0] == 8.50508193935929
+        assert results2.wd[0] == pytest.approx(8.50508193935929)
         assert results2.h[0] == pytest.approx(23.87158296249206)
         assert results2.p[0] == pytest.approx(0.3375435664999579)
         assert results2.o[0] == 0.5769230769230769
