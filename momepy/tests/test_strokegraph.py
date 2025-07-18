10b85e34927e69da2be0106b4d2e1b509ae44d7f
anvy <anvy@itu.dk>
2025-07-18 14:46:40 +0200

    update tests for strokegraph

--- momepy/tests/test_strokegraph.py
+++ momepy/tests/test_strokegraph.py
@@ -15,7 +15,7 @@ class TestStrokeGraph:
     def test_get_interior_angle(self):
 
         a = [172.43389124, 200.04361864]
-        b = [-21.00598791,53.213871]
+        b = [-21.00598791, 53.213871]
         result = mm.strokegraph._get_interior_angle(a, b)
         assert result == 62.30218235137648
         
@@ -39,11 +39,11 @@ class TestStrokeGraph:
         first_segment = np.array([0,-1])
         last_segment = np.array([-1,0])
         
-        assert mm.strokegraph._get_end_segment(linestring, [0,0]) == first_segment
-        assert mm.strokegraph._get_end_segment(linestring, [1,3]) == last_segment
+        assert all(mm.strokegraph._get_end_segment(linestring, [0,0]) == first_segment)
+        assert all(mm.strokegraph._get_end_segment(linestring, [1,3]) == last_segment)
 
         with pytest.raises(ValueError, match="point is not an endpoint of linestring!"):
             mm.strokegraph._get_end_segment(linestring, [0,2])
 
-    def test_make_stroke_graph(self):
+    def test_strokes_to_graph(self):
         pass
