f4ff378434549cd9cb79cadca6a8c9ee6b44c6a3
Clement Sebastiao <clement.sebastiao@gmail.com>
2025-12-10 17:31:41 -0500

    Update continuity name in tests

--- momepy/tests/test_continuity.py
+++ momepy/tests/test_continuity.py
@@ -18,12 +18,12 @@ class TestContinuityGraph:
     def test_get_interior_angle(self):
         a = [172.43389124, 200.04361864]
         b = [-21.00598791, 53.213871]
-        result = mm.strokegraph._get_interior_angle(a, b)
+        result = mm.continuity._get_interior_angle(a, b)
         assert result == 62.30218235137648
 
         a = [1, 0]
         b = [-1, 1]
-        result = mm.strokegraph._get_interior_angle(a, b)
+        result = mm.continuity._get_interior_angle(a, b)
         assert result == 45
 
     def test_get_end_segment(self):
@@ -32,8 +32,8 @@ class TestContinuityGraph:
         first_segment = np.array([0, -1])
         last_segment = np.array([-1, 0])
 
-        assert all(mm.strokegraph._get_end_segment(linestring, [0, 0]) == first_segment)
-        assert all(mm.strokegraph._get_end_segment(linestring, [1, 3]) == last_segment)
+        assert all(mm.continuity._get_end_segment(linestring, [0, 0]) == first_segment)
+        assert all(mm.continuity._get_end_segment(linestring, [1, 3]) == last_segment)
 
         with pytest.raises(ValueError, match="point is not an endpoint of linestring!"):
-            mm.strokegraph._get_end_segment(linestring, [0, 2])
+            mm.continuity._get_end_segment(linestring, [0, 2])
