c5f590453e542ebbe94d0fd6e319b3ecfea1951a
Martin Fleischmann <martin@martinfleischmann.net>
2026-07-14 11:41:52 +0800

    different float on macos...

--- momepy/shape.py
+++ momepy/shape.py
@@ -330,7 +330,7 @@ def courtyard_index(
     Length: 144, dtype: float64
 
     >>> ci.max()
-    np.float(0.16605915738643523)
+    np.float64(0.16605915738643523)
 
     If you know the courtyard area, you can pass it to skip the computation step.
 
